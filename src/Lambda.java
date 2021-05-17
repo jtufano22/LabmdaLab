7b9e03d310c0056676ffe9efe92ef2d449e22c27
Joseph <josephtufano03@gmail.com>
2021-05-17 13:26:04 -0400

    wee wee

--- src/Lambda.java
+++ src/Lambda.java
@@ -1,9 +1,7 @@
 public class Lambda {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Testing Git");
-        System.out.println("afaeijpf");
+
     }
 
 }
