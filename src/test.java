f1dbac3fac050db9044e9ca00fd2beabc81f8245
Joseph <josephtufano03@gmail.com>
2021-06-08 13:56:42 -0400

    bug fixes

--- src/test.java
+++ src/test.java
@@ -1,10 +1,10 @@
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class test {
-    public static void main(String[] args) {
-        HashMap<String, String> hi = new HashMap<>();
-        hi.put("a", "1");
-        System.out.println(hi.get("b"));
-    }
+public class test{
+
+
 }
