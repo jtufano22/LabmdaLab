6152ceaada37b6f008a0f1a61af7ce557367b1cf
mirza <strongsnow@gmail.com>
2021-06-10 15:41:31 -0400

    finished milestone 19, but minor issue when using run while setting a variable

--- src/Lambda.java
+++ src/Lambda.java
@@ -19,7 +19,8 @@ public class Lambda {
                 System.out.print(">");
                 input = in.nextLine();
                 continue;
-            } else if (semiColonPos >= 0) {
+            }
+            else if (semiColonPos >= 0) {
                 input = input.substring(0, semiColonPos).trim();
                 if (input.equals("")) {
                     System.out.print(">");
@@ -28,7 +29,12 @@ public class Lambda {
                 }
             }
 
-            if (input.contains("=")) {
+            if (input.contains("run ")) {
+                Parser p = new Parser(new Variable(input.substring(input.indexOf("run ")+4)));
+                String ret = run(p.print(p.tokens())).toString();
+                System.out.println(ret);
+            }
+            else if (input.contains("=")) {
                 String varName = input.substring(0, input.indexOf("=")).trim();
                 if (dictionary.get(varName) != null) {
                     System.out.print(varName + " is already defined\n>");
@@ -54,4 +60,28 @@ public class Lambda {
         System.out.println("Goodbye!");
     }
 
+    private static Expression run(Expression input) {
+        String in = input.toString();
+        int lambdaPos = in.indexOf('λ');
+        if(lambdaPos < 0) {
+            return input;
+        }
+        Parser p = new Parser(input);
+        int cPP = p.getCloseParenPos(0, in.substring(1, in.length()-1), new Stack<>());
+        if(cPP < 0) {
+            return input;
+        }
+        if(cPP + 1 + 2 == in.length()) {
+            return input;
+        }
+
+        String expr = in.substring(cPP+1 + 1, in.length()-1).trim();
+        String function = in.substring(1, cPP+1+1);
+        String functionVar = function.substring(2, function.indexOf('.'));
+        String ret = function.substring(function.indexOf('.')+1, function.length()-1).replaceAll(functionVar, expr);
+        if(ret.contains("λ") && ret.charAt(1) != 'λ') {
+            ret = run(new Variable(ret)).toString();
+        }
+        return new Variable(ret);
+    }
 }
